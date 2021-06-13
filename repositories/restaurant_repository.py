9bf4222838aa1da9abaeed08a322163ea7c00f56
Verbena <veve292@live.it>
2021-06-13 09:23:52 +0100

    add restaurant repository

---
+++ repositories/restaurant_repository.py
@@ -0,0 +1,42 @@
+from db.run_sql import run_sql
+from models.restaurant import Restaurant
+
+def save(restaurant):
+    sql = "INSERT INTO restaurants (name, category) VALUES (%s, %s) RETURNING id"
+    values = [restaurant.name, restaurant.category]
+    results = run_sql(sql, values)
+    restaurant.id = restaurant[0]['id']
+    return restaurant
+
+def select_all():
+    restaurants = []
+    sql = "SELECT * FROM restaurants"
+    results = run_sql(sql)
+
+    for row in results:
+        restaurant = Restaurant(row['name'], row['category'], row['id'])
+        restaurants.append(restaurant)
+    return restaurants
+
+def select(id):
+    restaurant = None
+    sql = "SELECT * FROM restaurants WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        restaurant = Restaurant(result['name'], result['category'], result['id'])
+    return restaurant
+
+def delete_all():
+    sql = "DELETE FROM restaurants"
+    run_sql(sql)
+
+def delete(id):
+    sql = "DELETE FROM restaurants WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
+
+
+
