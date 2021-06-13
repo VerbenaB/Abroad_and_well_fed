f5e5bf26e40fbc40ea24b912b6f16f038837bcb1
Verbena <veve292@live.it>
2021-06-13 14:36:26 +0100

    add index html to users

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -9,7 +9,7 @@ users_blueprint = Blueprint("users", __name__)
 @users_blueprint.route("/users")
 def users():
     users = user_repository.select_all()
-    return render_template("user/index.html", users = users)
+    return render_template("users/index.html", users = users)
 
 
 
