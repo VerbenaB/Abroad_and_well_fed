d503d9b914a9fd4f75a6d33a9e9642c7a7aa9341
Verbena <veve292@live.it>
2021-06-13 08:28:31 +0100

    first commit

---
+++ db/Italian_restaurants.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS reviews;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS restaurants;
+
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE restaurants (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    category VARCHAR(255)
+);
+
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    restaurant_id INT REFERENCES restaurants(id) ON DELETE CASCADE,
+    review TEXT
+);
+
